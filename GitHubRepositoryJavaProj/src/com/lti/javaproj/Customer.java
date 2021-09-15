f7295fbc4fafa6c24818ae3d3db7f21ae547822d
Prakash1707 <41486886+Prakash1707@users.noreply.github.com>
2021-09-15 15:28:13 +0530

    Create Customer.java

---
+++ GitHubRepositoryJavaProj/src/com/lti/javaproj/Customer.java
@@ -0,0 +1,35 @@
+package com.lti.javaproj;
+
+public class Customer {
+	private int id;
+	private String name;
+	private double bill;
+	public Product(int prdId, String prdName, double prdCost) {
+		super();
+		this.id = prdId;
+		this.name = prdName;
+		this.bill = prdCost;
+	}
+	@Override
+	public String toString() {
+		return "customer [id=" + id + ", name=" + name + ", bill=" + bill + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int prdId) {
+		this.id = prdId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String prdName) {
+		this.name = prdName;
+	}
+	public double getBill() {
+		return bill;
+	}
+	public void setBill(double prdCost) {
+		this.bill = prdCost;
+	}
+}
