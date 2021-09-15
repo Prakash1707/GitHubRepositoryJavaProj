78fbcbd2563516519edbc18585c3bb50ead6a0c8
Prakash1707 <saiprakashbaba1@gmail.com>
2021-09-15 15:22:33 +0530

    Added a main class

---
+++ GitHubRepositoryJavaProj/src/com/lti/javaproj/MyApp.java
@@ -0,0 +1,13 @@
+package com.lti.javaproj;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Product p=new Product(100,"Pen",1000);
+		System.out.println(p.toString());
+	}
+
+}
